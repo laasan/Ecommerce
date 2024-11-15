4d4d0fa70bf27cd21105a654ed38e0143b54318a
laasan <alekalx@gmail.com>
2024-11-15 17:59:06 +0545

    cart and qty in cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -35,7 +35,38 @@ function CartScreen() {
           </Message>
         ) : (
           <ListGroup variant='flush'>
+            {cartItems.map(item => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  
+                  <Col md={2}>
+                    {item.price}
+                  </Col>
 
+                  <Col md={3}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) => dispatch(addToCart(item.product, e.target.value))}
+                    >
+                      {
+                        [...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))
+                      }
+                    </Form.Control>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
           </ListGroup>
         )}
       </Col>
