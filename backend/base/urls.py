d2b896ac09927ca9873fa26c676cbd4feccb585f
laasan <alekalx@gmail.com>
2024-11-25 01:43:03 +0530

    protected routes

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(),
         name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
-    path('users/profile/', views.getUserProfile, name="user-profile"), # user-profile or users-profile ?
+    path('users/profile/', views.getUserProfile, name="users-profile"), # user-profile or users-profile ?
+    path('users/', views.getUsers, name="users"),
     path('products/', views.getProducts, name="products"),
     path('product/<str:pk>', views.getProduct, name="product"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-# from .products import products
+from django.contrib.auth.models import User
 from .models import Product
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
-
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -42,9 +42,17 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
