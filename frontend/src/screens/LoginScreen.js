d47977617221747dfe0b51c81ec117e4deb99381
laasan <alekalx@gmail.com>
2024-12-06 19:27:27 +0530

    FormContainer

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -4,6 +4,7 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import FormContainer from '../components/FormContainer'
 import { login } from '../actions/userActions'
 
 function LoginScreen() {
@@ -11,7 +12,7 @@ function LoginScreen() {
     const [password, setPassword] = useState('')
 
     return (
-        <div>LoginScreen</div>
+        <FormContainer>LoginScreen</FormContainer>
     )
 }
 
