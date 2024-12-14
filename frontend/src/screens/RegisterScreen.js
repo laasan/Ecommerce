8f030f5280568164c08b49af28bfe3a2206c76ae
laasan <alekalx@gmail.com>
2024-12-15 00:38:35 +0530

    update form preparation

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -35,7 +35,7 @@ function RegisterScreen() {
 
     const submitHandler = (e) => {
         e.preventDefault()
-        if(password != confirmPassword){
+        if(password !== confirmPassword){
             setMessage('Passwords do not match')
         } else {
             dispatch(register(name, email, password))
