87c35d1af19123f9b7f89752db70bdaee5501b14
laasan <alekalx@gmail.com>
2024-12-14 23:43:29 +0530

    user profile

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { getUserDetails } from '../actions/userActions'
 
 function ProfileScreen() {
   return (
-    <div>ProfileScreen</div>
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+      </Col>
+
+      <Col md={9}>
+        <h2>My Orders</h2>
+      </Col>
+
+    </Row>
   )
 }
 
