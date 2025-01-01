aa3bc640ed36de45d84ce06f38dd11666c53dcf9
laasan <alekalx@gmail.com>
2025-01-02 02:37:04 +0530

    start ShippingScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path='/login' Component={LoginScreen} />
             <Route path='/register' Component={RegisterScreen} />
             <Route path='/profile' Component={ProfileScreen} />
+            <Route path='/shipping' Component={ShippingScreen} />
             <Route path='/product/:id' Component={ProductScreen} />
             <Route path='/cart/:id?' Component={CartScreen} />
           </Routes>
