910e1442c6adcce8cdf72e3092f7245e62e75e2c
laasan <alekalx@gmail.com>
2025-08-18 18:40:05 +0545

    cart clear items

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -2,8 +2,8 @@ import {
     CART_ADD_ITEM,
     CART_REMOVE_ITEM,
     CART_SAVE_SHIPPING_ADDRESS,
-
-    CART_SAVE_PAYMENT_METHOD
+    CART_SAVE_PAYMENT_METHOD,
+    CART_CLEAR_ITEMS
 } from '../constants/cartConstants'
 
 
@@ -43,6 +43,12 @@ export const cartReducer = (state ={ cartItems: [], shippingAddress: {} }, actio
             return {
                 ...state,
                 paymentMethod: action.payload
+            }
+
+        case CART_CLEAR_ITEMS:
+            return {
+                ...state,
+                cartItems: []
             }
 
         default:
